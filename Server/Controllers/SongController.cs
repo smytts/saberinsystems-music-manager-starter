4a8ae9eeb4ef5e0f64e535a47bfd5863bef8b43b
John Brandt <john.brandt@saberin.com>
2024-10-01 21:04:11 -0400

    Added ability to delete songs from libraryAdd project files and readme

---
+++ Server/Controllers/SongController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using music_manager_starter.Data;
+using music_manager_starter.Data.Models;
+using System;
+
+namespace music_manager_starter.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private readonly DataDbContext _context;
+
+        public SongsController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+  
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Song>>> GetSongs()
+        {
+            return await _context.Songs.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Song>> PostSong(Song song)
+        {
+            if (song == null)
+            {
+                return BadRequest("Song cannot be null.");
+            }
+
+
+            _context.Songs.Add(song);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSong(Guid id)
+        {
+            // Find the song by ID
+            var song = await _context.Songs.FindAsync(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            // Remove the song from the context
+            _context.Songs.Remove(song);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
